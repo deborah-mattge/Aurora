a008a756abd62737d7540cde8e5d59fe60fc7210
Heloisa Fogaca do Nascimento <heloisafn@weg.net>
2024-04-26 21:56:36 -0300

    feat: adding new inputs

--- lib/pages/create_habit.dart
+++ lib/pages/create_habit.dart
@@ -20,6 +20,9 @@ class MyHomePage extends StatefulWidget {
 class _MyHomePageState extends State<MyHomePage> {
   final controller = TextEditingController();
 
+  final dropValue = ValueNotifier('');
+  final dropOptions = ['Sim/Não', 'Quantidade', 'Tempo'];
+
   @override
   void initState() {
     super.initState();
@@ -29,82 +32,207 @@ class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: const Text('AURORA'),
-          backgroundColor: const Color.fromRGBO(61, 170, 243, 0.522),
-        ),
-        body: Padding(
-          padding: const EdgeInsets.all(20),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              const Padding(
-                padding: EdgeInsets.only(top: 30.0),
-                child: Center(
-                  child: Text(
-                    'Novo Hábito',
-                    style: TextStyle(
-                        fontSize: 22,
-                        fontWeight: FontWeight.w600,
-                        color: Color.fromRGBO(74, 74, 73, 1)),
+      appBar: AppBar(
+        title: const Text('AURORA'),
+        backgroundColor: const Color.fromRGBO(61, 170, 243, 0.522),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(20),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            const Padding(
+              padding: EdgeInsets.only(top: 30.0),
+              child: Center(
+                child: Text(
+                  'Novo Hábito',
+                  style: TextStyle(
+                    fontSize: 22,
+                    fontWeight: FontWeight.w600,
+                    color: Color.fromRGBO(74, 74, 73, 1),
                   ),
                 ),
               ),
-              Padding(
-                  padding:
-                      const EdgeInsets.only(top: 30.0, left: 25.0, right: 25.0),
-                  child: Column(
+            ),
+            Padding(
+              padding:
+                  const EdgeInsets.only(top: 30.0, left: 25.0, right: 25.0),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  const Row(
                     children: [
-                      const Row(
-                        children: [
-                          Text(
-                            'Nome',
-                            style: TextStyle(
-                                fontSize: 16,
-                                fontWeight: FontWeight.w400,
-                                color: Color.fromRGBO(74, 74, 73, 1)),
-                          ),
-                          SizedBox(width: 3),
-                          Text(
-                            '*',
-                            style: TextStyle(
-                                fontSize: 18,
-                                fontWeight: FontWeight.w400,
-                                color: Color.fromRGBO(255, 71, 117, 1)),
-                          ),
-                        ],
+                      Text(
+                        'Nome',
+                        style: TextStyle(
+                          fontSize: 16,
+                          fontWeight: FontWeight.w400,
+                          color: Color.fromRGBO(74, 74, 73, 1),
+                        ),
                       ),
-                      TextField(
-                        decoration: getHabitInputDecorations(
-                            sendText: 'Ex: Beber água',
-                            vertical: 10,
-                            horizontal: 15,
-                            width: 0),
+                      SizedBox(width: 3, height: 2),
+
+                      // Nome do hábito
+                      Text(
+                        '*',
+                        style: TextStyle(
+                          fontSize: 18,
+                          fontWeight: FontWeight.w400,
+                          color: Color.fromRGBO(255, 71, 117, 1),
+                        ),
                       ),
-                      const SizedBox(height: 20),
-                      const Row(
-                          mainAxisAlignment: MainAxisAlignment.start,
-                          children: [
-                            Text(
-                              '*',
-                              style: TextStyle(
-                                  fontSize: 18,
-                                  fontWeight: FontWeight.w400,
-                                  color: Color.fromRGBO(255, 71, 117, 1)),
-                            )
-                          ]),
-                      TextField(
-                        decoration: getHabitInputDecorations(
-                            sendText: 'Tipo',
-                            vertical: 10,
-                            horizontal: 10,
-                            width: 5),
-                      )
                     ],
-                  ))
-            ],
-          ),
+                  ),
+                  TextField(
+                    // INPUT 'NOME'
+                    decoration: getHabitInputDecorations(
+                      sendText: 'Ex: Beber água',
+                      vertical: 10,
+                      horizontal: 15,
+                      width: 0,
+                    ),
+                  ),
+                  const SizedBox(height: 20),
+
+
+                  
+                  const Row(
+                    children: [
+                      Text(
+                        '*',
+                        style: TextStyle(
+                          fontSize: 18,
+                          fontWeight: FontWeight.w400,
+                          color: Color.fromRGBO(255, 71, 117, 1),
+                        ),
+                      ),
+                      SizedBox(width: 159),
+                      Text(
+                        '*',
+                        style: TextStyle(
+                          fontSize: 18,
+                          fontWeight: FontWeight.w400,
+                          color: Color.fromRGBO(255, 71, 117, 1),
+                        ),
+                      ),
+                    ],
+                  ),
+                  Row(
+                    children: [
+                      Expanded(
+                        child: Row(
+                          children: [
+                            const SizedBox(width: 0),
+                            Expanded(
+                                child: ValueListenableBuilder<String>(
+                              valueListenable: dropValue,
+                              builder: (BuildContext context, String value, _) {
+                                return DropdownButtonFormField<String>(
+                                  decoration: InputDecoration(
+                                    hintStyle: const TextStyle(
+                                        fontSize: 15, color: Colors.black38),
+                                    contentPadding: const EdgeInsets.symmetric(
+                                      vertical: 10,
+                                      horizontal: 10,
+                                    ),
+                                    border: OutlineInputBorder(
+                                      borderRadius: BorderRadius.circular(8),
+                                    ),
+                                    enabledBorder: OutlineInputBorder(
+                                      borderRadius: BorderRadius.circular(8),
+                                      borderSide: const BorderSide(
+                                        color: Colors.black12,
+                                        width: 1,
+                                      ),
+                                    ),
+                                  ),
+                                  icon: const Icon(
+                                    Icons.keyboard_arrow_down,
+                                    color: Color.fromARGB(255, 79, 196, 248),
+                                    size: 25,
+                                  ),
+                                  hint: const Text('Tipo'),
+                                  value: (value.isEmpty) ? null : value,
+                                  onChanged: (choice) =>
+                                      dropValue.value = choice.toString(),
+                                  items: dropOptions
+                                      .map((op) => DropdownMenuItem(
+                                            value: op,
+                                            child: Text(
+                                              op,
+                                              style: const TextStyle(
+                                                  color: Color.fromRGBO(
+                                                      88, 88, 88, 1)),
+                                            ),
+                                          ))
+                                      .toList(),
+                                );
+                              },
+                            )),
+                          ],
+                        ),
+                      ),
+                      const SizedBox(width: 30),
+                      Expanded(
+                        child: Row(
+                          children: [
+                            const SizedBox(width: 0),
+                            Expanded(
+                              // INPUT 'EX: LITROS'
+                              child: TextField(
+                                decoration: getHabitInputDecorations(
+                                  sendText: 'Ex: Litros',
+                                  vertical: 10,
+                                  horizontal: 10,
+                                  width: 5,
+                                ),
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ],
+                  ),
+                  const Padding(
+                    padding: EdgeInsets.only(top: 30.0),
+                    child: Row(
+                      children: [
+                        Expanded(
+                          child: Stack(
+                            children: [
+                              Text(
+                                "Escolha uma tag",
+                                style: TextStyle(
+                                  fontSize: 16,
+                                  fontWeight: FontWeight.w400,
+                                  color: Color.fromRGBO(74, 74, 73, 1),
+                                ),
+                              ),
+                              Positioned(
+                                bottom: -1,
+                                left: 125.2,
+                                child: Text(
+                                  '*',
+                                  style: TextStyle(
+                                    fontSize: 18,
+                                    fontWeight: FontWeight.w400,
+                                    color: Color.fromRGBO(255, 71, 117, 1),
+                                  ),
+                                ),
+                              )
+                            ],
+                          ),
+                        ),
+                      ],
+                    ),
+                  )
+                ],
+              ),
+            ),
+          ],
         ),
-        backgroundColor: const Color.fromRGBO(245, 245, 245, 1.0));
+      ),
+      backgroundColor: const Color.fromRGBO(245, 245, 245, 1.0),
+    );
   }
 }
